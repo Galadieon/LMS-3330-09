98bcdab4937040bd41dd5ef66e182b030a7d3820
Galadieon <lnn7447@mavs.uta.edu>
2025-04-29 23:34:45 -0500

    modified lms sql

--- SSENTONGO_SHAH_NINH/Code/LMS.sql
+++ SSENTONGO_SHAH_NINH/Code/LMS.sql
@@ -19,10 +19,10 @@
 -- Table structure for table `Book`
 --
 
-DROP DATABASE IF EXISTS LMS;
-CREATE DATABASE LMS; -- Library Managemente System
+-- DROP DATABASE IF EXISTS LMS;
+-- CREATE DATABASE LMS; -- Library Managemente System
 
-USE LMS;
+-- USE LMS;
 
 DROP TABLE IF EXISTS `Book`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -34,7 +34,7 @@ CREATE TABLE `Book` (
   PRIMARY KEY (`book_id`),
   KEY `book_publisher` (`book_publisher`),
   CONSTRAINT `Book_ibfk_1` FOREIGN KEY (`book_publisher`) REFERENCES `Publisher` (`publisher_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +43,7 @@ CREATE TABLE `Book` (
 
 LOCK TABLES `Book` WRITE;
 /*!40000 ALTER TABLE `Book` DISABLE KEYS */;
-INSERT INTO `Book` VALUES (1,'To Kill a Mockingbird','HarperCollins'),(2,'1984','Penguin Books'),(3,'Pride and Prejudice','Penguin Classics'),(4,'The Great Gatsby','Scribner'),(5,'One Hundred Years of Solitude','Harper & Row'),(6,'Animal Farm','Penguin Books'),(7,'The Catcher in the Rye','Little, Brown and Company'),(8,'Lord of the Flies','Faber and Faber'),(9,'Brave New World','Chatto & Windus'),(10,'The Picture of Dorian Gray','Ward, Lock and Co.'),(11,'The Alchemist','HarperCollins'),(12,'The God of Small Things','Random House India'),(13,'Wuthering Heights','Thomas Cautley Newby'),(14,'The Hobbit','Allen & Unwin'),(15,'The Lord of the Rings','Allen & Unwin'),(16,'The Hitchhiker\'s Guide to the Galaxy','Pan Books'),(17,'The Diary of a Young Girl','Bantam Books'),(18,'The Da Vinci Code','Doubleday'),(19,'The Adventures of Huckleberry Finn','Penguin Classics'),(20,'The Adventures of Tom Sawyer','American Publishing Company'),(21,'A Tale of Two Cities','Chapman and Hall'),(999,'Harry Potter and the Sorcerer\'s Stone','Oxford Publishing');
+INSERT INTO `Book` VALUES (1,'To Kill a Mockingbird','HarperCollins'),(2,'1984','Penguin Books'),(3,'Pride and Prejudice','Penguin Classics'),(4,'The Great Gatsby','Scribner'),(5,'One Hundred Years of Solitude','Harper & Row'),(6,'Animal Farm','Penguin Books'),(7,'The Catcher in the Rye','Little, Brown and Company'),(8,'Lord of the Flies','Faber and Faber'),(9,'Brave New World','Chatto & Windus'),(10,'The Picture of Dorian Gray','Ward, Lock and Co.'),(11,'The Alchemist','HarperCollins'),(12,'The God of Small Things','Random House India'),(13,'Wuthering Heights','Thomas Cautley Newby'),(14,'The Hobbit','Allen & Unwin'),(15,'The Lord of the Rings','Allen & Unwin'),(16,'The Hitchhiker\'s Guide to the Galaxy','Pan Books'),(17,'The Diary of a Young Girl','Bantam Books'),(18,'The Da Vinci Code','Doubleday'),(19,'The Adventures of Huckleberry Finn','Penguin Classics'),(20,'The Adventures of Tom Sawyer','American Publishing Company'),(21,'A Tale of Two Cities','Chapman and Hall');
 /*!40000 ALTER TABLE `Book` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -68,7 +68,7 @@ CREATE TABLE `Book_Authors` (
 
 LOCK TABLES `Book_Authors` WRITE;
 /*!40000 ALTER TABLE `Book_Authors` DISABLE KEYS */;
-INSERT INTO `Book_Authors` VALUES (1,'Harper Lee'),(2,'George Orwell'),(3,'Jane Austen'),(4,'F. Scott Fitzgerald'),(5,'Gabriel Garcia Marquez'),(6,'George Orwell'),(7,'J.D. Salinger'),(8,'William Golding'),(9,'Aldous Huxley'),(10,'Oscar Wilde'),(11,'Paulo Coelho'),(12,'Arundhati Roy'),(13,'Emily Bronte'),(14,'J.R.R. Tolkien'),(15,'J.R.R. Tolkien'),(16,'Douglas Adams'),(17,'Anne Frank'),(18,'Dan Brown'),(19,'Mark Twain'),(20,'Mark Twain'),(21,'Charles Dickens'),(999,'J.K. Rowling');
+INSERT INTO `Book_Authors` VALUES (1,'Harper Lee'),(2,'George Orwell'),(3,'Jane Austen'),(4,'F. Scott Fitzgerald'),(5,'Gabriel Garcia Marquez'),(6,'George Orwell'),(7,'J.D. Salinger'),(8,'William Golding'),(9,'Aldous Huxley'),(10,'Oscar Wilde'),(11,'Paulo Coelho'),(12,'Arundhati Roy'),(13,'Emily Bronte'),(14,'J.R.R. Tolkien'),(15,'J.R.R. Tolkien'),(16,'Douglas Adams'),(17,'Anne Frank'),(18,'Dan Brown'),(19,'Mark Twain'),(20,'Mark Twain'),(21,'Charles Dickens');
 /*!40000 ALTER TABLE `Book_Authors` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,7 +96,7 @@ CREATE TABLE `Book_Copies` (
 
 LOCK TABLES `Book_Copies` WRITE;
 /*!40000 ALTER TABLE `Book_Copies` DISABLE KEYS */;
-INSERT INTO `Book_Copies` VALUES (1,1,3),(2,1,2),(3,2,1),(4,3,5),(5,1,5),(6,2,3),(7,2,2),(8,3,2),(9,1,4),(10,2,2),(11,1,3),(12,3,3),(13,3,2),(14,1,5),(15,3,2),(16,2,3),(17,3,3),(18,3,3),(19,1,5),(20,3,2),(21,3,2);
+INSERT INTO `Book_Copies` VALUES (1,1,3),(2,1,2),(3,2,1),(4,3,4),(5,1,5),(6,2,3),(7,2,2),(8,3,1),(9,1,4),(10,2,2),(11,1,3),(12,3,2),(13,3,1),(14,1,5),(15,3,1),(16,2,3),(17,3,2),(18,3,2),(19,1,5),(20,3,1),(21,3,1);
 /*!40000 ALTER TABLE `Book_Copies` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -147,7 +147,7 @@ CREATE TABLE `Borrower` (
   `address` varchar(255) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`card_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=989900 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=989899 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -156,7 +156,7 @@ CREATE TABLE `Borrower` (
 
 LOCK TABLES `Borrower` WRITE;
 /*!40000 ALTER TABLE `Borrower` DISABLE KEYS */;
-INSERT INTO `Borrower` VALUES (111111,'Alex Kim','983 Sine St, Arizona, AR 70451','678-784-5563'),(121212,'Chloe Park','345 Shark St, Arizona, AR 72213','755-905-5572'),(123456,'John Smith','456 Oak St, Arizona, AR 70010','205-555-5555'),(222222,'Rachel Lee','999 Apple Ave, Arizona, AR 70671','231-875-5564'),(232323,'William Chen','890 Sting St, New York, NY 10459','406-755-5580'),(234567,'Emily Lee','389 Oaklay St, Arizona, AR 70986','231-678-5560'),(333333,'William Johnson','705 Paster St, New Jersey 32002','235-525-5567'),(343434,'Olivia Johnson','345 Pine St, New Jersey, NJ 32095','662-554-5575'),(345678,'Bob Johnson','12 Elm St, Arizona, AR 70345 ','545-234-5557'),(444444,'Ethan Martinez','466 Deeplm St, New York, NY 10321','555-555-5569'),(454545,'Dylan Kim','567 Cowboy way St, New Jersey, NJ 32984','435-254-5578'),(456789,'Laura Chen','345 Mapman Ave, Arizona, AR 70776','565-985-9962'),(555555,'Grace Hernandez','315 Babes St, Arizona, AR 70862 ','455-567-5587'),(565656,'Sophia Park','678 Dolphin St, New York, NY 10062','675-455-5568'),(567890,'Tom Lee','678  S Oak St, New York, NY 10045','209-525-5559'),(676767,'Olivia Lee','345 Spine St, New York, NY 10092','435-878-5569'),(787878,'Noah Thompson','189 GreenOak Ave, New Jersey, NJ 32453','245-555-5571'),(789012,'Jane Doe','789 Maple Ave, New Jersey, NJ 32542','555-235-5556'),(890123,'Michael Park','123 Pinewood St, New Jersey, NJ 32954','655-890-2161'),(901234,'Sarah Kim','345 Pine St, New York, NY 10065','515-325-2158'),(989898,'Olivia Smith','178 Elm St, New Jersey, NJ 32124','325-500-5579'),(989899,'Alice Cooper','999 Unity St, Plano, TX','(837) 721-8965');
+INSERT INTO `Borrower` VALUES (111111,'Alex Kim','983 Sine St, Arizona, AR 70451','678-784-5563'),(121212,'Chloe Park','345 Shark St, Arizona, AR 72213','755-905-5572'),(123456,'John Smith','456 Oak St, Arizona, AR 70010','205-555-5555'),(222222,'Rachel Lee','999 Apple Ave, Arizona, AR 70671','231-875-5564'),(232323,'William Chen','890 Sting St, New York, NY 10459','406-755-5580'),(234567,'Emily Lee','389 Oaklay St, Arizona, AR 70986','231-678-5560'),(333333,'William Johnson','705 Paster St, New Jersey 32002','235-525-5567'),(343434,'Olivia Johnson','345 Pine St, New Jersey, NJ 32095','662-554-5575'),(345678,'Bob Johnson','12 Elm St, Arizona, AR 70345 ','545-234-5557'),(444444,'Ethan Martinez','466 Deeplm St, New York, NY 10321','555-555-5569'),(454545,'Dylan Kim','567 Cowboy way St, New Jersey, NJ 32984','435-254-5578'),(456789,'Laura Chen','345 Mapman Ave, Arizona, AR 70776','565-985-9962'),(555555,'Grace Hernandez','315 Babes St, Arizona, AR 70862 ','455-567-5587'),(565656,'Sophia Park','678 Dolphin St, New York, NY 10062','675-455-5568'),(567890,'Tom Lee','678  S Oak St, New York, NY 10045','209-525-5559'),(676767,'Olivia Lee','345 Spine St, New York, NY 10092','435-878-5569'),(787878,'Noah Thompson','189 GreenOak Ave, New Jersey, NJ 32453','245-555-5571'),(789012,'Jane Doe','789 Maple Ave, New Jersey, NJ 32542','555-235-5556'),(890123,'Michael Park','123 Pinewood St, New Jersey, NJ 32954','655-890-2161'),(901234,'Sarah Kim','345 Pine St, New York, NY 10065','515-325-2158'),(989898,'Olivia Smith','178 Elm St, New Jersey, NJ 32124','325-500-5579');
 /*!40000 ALTER TABLE `Borrower` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -182,7 +182,7 @@ CREATE TABLE `Library_Branch` (
 
 LOCK TABLES `Library_Branch` WRITE;
 /*!40000 ALTER TABLE `Library_Branch` DISABLE KEYS */;
-INSERT INTO `Library_Branch` VALUES (1,'Main Branch','123 Main St, New York, NY 10003',1.20),(2,'West Branch','456 West St, Arizona, AR 70622',0.75),(3,'East Branch','789 East St, New Jersy, NY 32032',2.30),(4,'North Branch','456 NW, Irving, TX 76100',0.50),(5,'UTA Branch','123 Cooper St, Arlington TX 76101',0.50);
+INSERT INTO `Library_Branch` VALUES (1,'Main Branch','123 Main St, New York, NY 10003',1.20),(2,'West Branch','456 West St, Arizona, AR 70622',0.75),(3,'East Branch','789 East St, New Jersy, NY 32032',2.30);
 /*!40000 ALTER TABLE `Library_Branch` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -207,7 +207,7 @@ CREATE TABLE `Publisher` (
 
 LOCK TABLES `Publisher` WRITE;
 /*!40000 ALTER TABLE `Publisher` DISABLE KEYS */;
-INSERT INTO `Publisher` VALUES ('Allen & Unwin','212-782-9001','22 New Wharf Rd, Arizona, AR 70654'),('American Publishing Company','682-243-3524','7652 Northgate way lane, Georgia, GA 30054'),('Bantam Books','313-243-5354','1745 Broadway, New York, NY 10019'),('Chapman and Hall','833-342-2343','789 Oak St, Texas, TX 76010'),('Chatto & Windus','442-727-3800','Bloomsbury House, 7477 Great Russell St, Arizona, AR 72965'),('Doubleday','212-782-9000','789 Division St, Minnesota, MN 55344'),('Faber and Faber','201-797-3800','463 south centre street, Arizona, AR 71653'),('Harper & Row','212-207-7000','1195 Border street, Montana, MT 59007'),('HarperCollins','212-207-7000','195 Broadway, New York, NY 10007'),('Little, Brown and Company','212-764-2000','111 Huddle St, New Jersey, NJ 32014'),('Oxford Publishing','(111) 222-3333','123 Oxford St, UK'),('Pan Books','313-243-5353','567 Pine Tree Rd, Colorado, CO 87348'),('Penguin Books','212-366-3000','475 Hudson St, New York, NY 10014'),('Penguin Classics','212-366-2000','123 Main St, California, CA 01383'),('Random House India','291-225-6634','423 baywatch centre street, Alabama, AL 30513'),('Scribner','212-207-7474','19 Broadway, New York, NY 10007'),('Thomas Cautley Newby','243-353-2352','890 Elmwood Dr, Floride, FL 98238'),('Ward, Lock and Co.','647-242-3434','456 Maple Ave, Texas, TX 76013 ');
+INSERT INTO `Publisher` VALUES ('Allen & Unwin','212-782-9001','22 New Wharf Rd, Arizona, AR 70654'),('American Publishing Company','682-243-3524','7652 Northgate way lane, Georgia, GA 30054'),('Bantam Books','313-243-5354','1745 Broadway, New York, NY 10019'),('Chapman and Hall','833-342-2343','789 Oak St, Texas, TX 76010'),('Chatto & Windus','442-727-3800','Bloomsbury House, 7477 Great Russell St, Arizona, AR 72965'),('Doubleday','212-782-9000','789 Division St, Minnesota, MN 55344'),('Faber and Faber','201-797-3800','463 south centre street, Arizona, AR 71653'),('Harper & Row','212-207-7000','1195 Border street, Montana, MT 59007'),('HarperCollins','212-207-7000','195 Broadway, New York, NY 10007'),('Little, Brown and Company','212-764-2000','111 Huddle St, New Jersey, NJ 32014'),('Pan Books','313-243-5353','567 Pine Tree Rd, Colorado, CO 87348'),('Penguin Books','212-366-3000','475 Hudson St, New York, NY 10014'),('Penguin Classics','212-366-2000','123 Main St, California, CA 01383'),('Random House India','291-225-6634','423 baywatch centre street, Alabama, AL 30513'),('Scribner','212-207-7474','19 Broadway, New York, NY 10007'),('Thomas Cautley Newby','243-353-2352','890 Elmwood Dr, Floride, FL 98238'),('Ward, Lock and Co.','647-242-3434','456 Maple Ave, Texas, TX 76013 ');
 /*!40000 ALTER TABLE `Publisher` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -259,4 +259,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-29 21:19:31
+-- Dump completed on 2025-04-29 23:32:56
